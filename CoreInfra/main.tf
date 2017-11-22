c287decd195efd69927405363dfe0c99f323462a
delosric <delosric@yahoo.com>
2017-11-22 15:43:30 +0100

    ressources provider vpc subnet

---
+++ CoreInfra/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "172.23.0.0/16"
+
+  tags {
+    Name = "Simon"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "172.23.1.0/24"
+
+  tags {
+    Name = "Simon"
+  }
+}
