dfc70fca5203a2bb4ccd75cde669739e114d68ce
delosric <delosric@yahoo.com>
2017-11-22 16:10:23 +0100

    ressource route_table

--- CoreInfra/main.tf
+++ CoreInfra/main.tf
@@ -26,3 +26,16 @@ resource "aws_internet_gateway" "main" {
     Name = "Simon"
   }
 }
+
+resource "aws_route_table" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main.id}"
+  }
+
+  tags {
+    Name = "Simon"
+  }
+}
