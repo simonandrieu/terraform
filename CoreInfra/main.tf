8196a95d3326b6bdf38236739e7cfb327db71e40
delosric <delosric@yahoo.com>
2017-11-22 16:25:40 +0100

    ressource route table association

--- CoreInfra/main.tf
+++ CoreInfra/main.tf
@@ -39,3 +39,8 @@ resource "aws_route_table" "main" {
     Name = "Simon"
   }
 }
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = "${aws_subnet.main.id}"
+  route_table_id = "${aws_route_table.main.id}"
+}
